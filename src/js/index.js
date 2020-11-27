ead2aa8f5a2def00955c7f6b0ed78e8732bb77c0
Jakub Wierzchowski <wierzcho155@gmail.com>
2020-11-27 16:28:19 +0100

    darkmode

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,9 @@
 import '../scss/main.scss';
 
-
+import moment from 'moment';
+const startOfDay = moment().startOf('day').fromNow();
+const timePlaceholder = document.querySelector('.time--js');
+timePlaceholder.innerHTML = startOfDay;
 
 // uncomment the lines below to enable PWA
 // import {registerSW} from './pwa.js';
@@ -9,3 +12,22 @@ import '../scss/main.scss';
 /* place your code below */
 
 console.log('HELLO 🚀')
+
+
+
+
+let isDark = false;
+const switchModes = document.querySelector('.mode--js')
+
+switchModes.addEventListener('click', () => {
+  if (isDark) {
+    document.documentElement.style.setProperty('--background-color', '#fefefe');
+    document.documentElement.style.setProperty('--color', 'black');
+    isDark = false;
+  } else {
+    document.documentElement.style.setProperty('--background-color', 'black');
+    document.documentElement.style.setProperty('--color', 'yellow');
+    isDark = true;
+  }
+  
+})
